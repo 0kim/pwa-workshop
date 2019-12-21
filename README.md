0da669f96346945c29af9827720d944dee7be464
I351009 <young.hun.kim@sap.com>
2019-12-21 16:01:03 +0900

    added service worker

---
+++ README.md
@@ -0,0 +1,3 @@
+practice with 
+* https://github.com/techhtml/2019-12-21-pwa-workshop
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 
-
 const App = () => (
     <div className="app">
-        Hello Azure~~~~ and PWA~~~~~111
+        Hello Azure~ and PWA~ 2
     </div>
 )
 
 ReactDOM.render(<App />, document.getElementById("app"))
+
+if('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js');   // 'service-worker.js' 는 GenerateSW가 생성해줌 
+    })
+}
